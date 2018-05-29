332306039263acec1aa2450ca3006164499ff64a
2fx0one <2fx0one@gmail.com>
2018-05-29 16:56:09 +0800

    修改一下mapper的配置值。用application配置的

--- src/main/java/com/tfx0one/web/mapper/DemoModelMapper.java
+++ src/main/java/com/tfx0one/web/mapper/DemoModelMapper.java
@@ -2,8 +2,6 @@ package com.tfx0one.web.mapper;
 
 import com.tfx0one.common.util.MyMapper;
 import com.tfx0one.web.model.DemoModel;
-import org.springframework.cache.annotation.CacheConfig;
-
 
 public interface DemoModelMapper extends MyMapper<DemoModel> {
 }

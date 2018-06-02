935e08f84ccc97ee5bc91d0acef2bb161f575d9b
2fx0one <2fx0one@gmail.com>
2018-06-02 10:16:33 +0800

    微信配置删除

--- src/main/java/com/tfx0one/common/constant/WXAPIConstant.java
+++ src/main/java/com/tfx0one/common/constant/WXAPIConstant.java
@@ -5,10 +5,6 @@ package com.tfx0one.common.constant;
  */
 
 public class WXAPIConstant {
-//    wxapp.jscode2session=https://api.weixin.qq.com/sns/jscode2session
-//    wxapp.appId=wxdda83d03c2d1521c
-//    wxapp.secret=fad3978bb8d6aed7a341feb506b5f6e5
-//    wxapp.grantType=authorization_code
     public static final String URL_JSCODE2SESSION = "https://api.weixin.qq.com/sns/jscode2session";
 
 

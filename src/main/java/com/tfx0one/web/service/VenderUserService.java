22cd363f16d5a7e25501e97be13d40df2d5ca14b
2fx0one <2fx0one@gmail.com>
2018-06-04 09:32:29 +0800

    update

---
+++ src/main/java/com/tfx0one/web/service/VenderUserService.java
@@ -0,0 +1,14 @@
+package com.tfx0one.web.service;
+
+import com.tfx0one.common.util.BaseService;
+import com.tfx0one.web.model.VendorUser;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by 2fx0one on 2018/6/1.
+ */
+
+@Service
+public class VenderUserService extends BaseService<VendorUser> {
+
+}

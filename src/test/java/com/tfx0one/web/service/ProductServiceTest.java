dc7462b9707a19fcbb2b70ccc525c682436cb051
lihongyu <yulihong.1990@163.com>
2018-06-08 15:43:53 +0800

    添加产品测试类

---
+++ src/test/java/com/tfx0one/web/service/ProductServiceTest.java
@@ -0,0 +1,24 @@
+package com.tfx0one.web.service;
+
+import com.tfx0one.web.model.EShopProduct;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductServiceTest {
+
+ //   @Autowired
+//    public ProductService productService;
+
+//    @Test
+ //   public void insertProductData()  {
+
+ //       int i = productService.insertProductData(new EShopProduct());
+   // }
+}

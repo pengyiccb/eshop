e2480e5f74f9db3a35d531155cfd089089f66dd4
lihongyu <yulihong.1990@163.com>
2018-06-09 11:03:29 +0800

    商品添加

--- src/main/java/com/tfx0one/web/mapper/EShopProductCategoryMapper.java
+++ src/main/java/com/tfx0one/web/mapper/EShopProductCategoryMapper.java
@@ -2,7 +2,9 @@ package com.tfx0one.web.mapper;
 
 import com.tfx0one.common.util.MyMapper;
 import com.tfx0one.web.model.EShopProductCategory;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface EShopProductCategoryMapper extends MyMapper<EShopProductCategory> {
-    public int insertEShopCategoryAndGetID (EShopProductCategory eshopProductCategory);
+    int insertEShopCategoryAndGetID (EShopProductCategory eshopProductCategory);
 }

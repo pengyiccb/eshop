7b4256d41393b62165cf779b4fd61f84bee7c386
2fx0one <2fx0one@gmail.com>
2018-06-11 15:56:04 +0800

    开启事务支持

--- src/main/java/com/tfx0one/SpringBootEShopApplication.java
+++ src/main/java/com/tfx0one/SpringBootEShopApplication.java
@@ -7,9 +7,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
+import java.lang.annotation.Target;
+
 @SpringBootApplication
+
+//关闭JDK动态代理机制
+@EnableTransactionManagement(proxyTargetClass = true)
 @MapperScan(basePackages = "com.tfx0one.web.mapper")
 public class SpringBootEShopApplication {
 

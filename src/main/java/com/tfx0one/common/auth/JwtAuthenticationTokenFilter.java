826b3a33f526cfa772bf86329ce5250cd024a863
2fx0one <2fx0one@gmail.com>
2018-06-15 16:20:04 +0800

    update

--- src/main/java/com/tfx0one/common/auth/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/tfx0one/common/auth/JwtAuthenticationTokenFilter.java
@@ -42,40 +42,35 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
         //拿到token 进行验证
         String authHeader = request.getHeader(this.tokenHeader);
         if (authHeader != null && authHeader.startsWith(tokenHead) && authHeader.length()>tokenHead.length() + 1) {
-            logger.info("authentication authHeader = [ " + authHeader + " ]");
+//            logger.info("authentication authHeader = [ " + authHeader + " ]");
             final String authToken = authHeader.substring(tokenHead.length() + 1); // The part after "Bearer " 用空格
             String username = jwtTokenUtils.getUsernameFromToken(authToken);
 
             logger.info("authentication username = " + username);
+            //TODO 验证失败时。需要返回信息
             if (username == null) {
                 errorStrWriteToResponse(response, -1, "无效的 Token");
                 return;
             }
 
-            //TODO 验证失败时。需要返回信息
-
+            //加入到授权上下文
             if (SecurityContextHolder.getContext().getAuthentication() == null) {
                 logger.info("checking authentication ===== " + username);
 
                 UserDetails userDetails = jwtUserService.loadUserByUsername(username);
 
                 if (jwtTokenUtils.validateToken(authToken, userDetails)) {
-//                    logger.info("checking authentication 1===== ");
-                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                            userDetails, null, userDetails.getAuthorities());
+                    UsernamePasswordAuthenticationToken authentication
+                            = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
-//                    logger.info("checking authentication 2===== ");
-                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(
-                            request));
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
                     logger.info("checking authentication =====  authenticated user " + username + " setting security context");
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
             }
         }
-
         filterChain.doFilter(request, response);
-
     }
 
     private void errorStrWriteToResponse(HttpServletResponse response, int code, String errorCode) throws IOException {

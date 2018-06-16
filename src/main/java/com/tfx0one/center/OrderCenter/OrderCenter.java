f0a75cac65d875c3ce6922d65516a4f672d5aa65
2fx0one <2fx0one@gmail.com>
2018-06-16 14:17:14 +0800

    add

---
+++ src/main/java/com/tfx0one/center/OrderCenter/OrderCenter.java
@@ -0,0 +1,7 @@
+package com.tfx0one.center.OrderCenter;
+
+/**
+ * Created by 2fx0one on 2018/6/16.
+ */
+public class OrderCenter {
+}
---
+++ src/main/java/com/tfx0one/center/VendorCenter/Vendor.java
@@ -0,0 +1,7 @@
+package com.tfx0one.center.VendorCenter;
+
+/**
+ * Created by 2fx0one on 2018/6/16.
+ */
+public class Vendor {
+}
